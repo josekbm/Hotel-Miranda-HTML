a56efe25d6549f59a9977d5097eda046e889279a
josekbm <jcblanco91@outlook.com>
2023-06-20 13:47:12 +0200

    Home finished

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,17 +1,7 @@
-const swiper = new Swiper('.swiper--handpicked-rooms', {
-   direction: "horizontal",
-   loop: true,
+const menuPrevButton = document.querySelector('.menu__button-prev');
+const menuNextButton = document.querySelector('.menu__button-next');
 
-   navigation: {
-    nextEl: nextButton,
-    prevEl: prevButton, 
-  },
-
-   
-});
-
-
-  const swiperCoreFeatures = new Swiper('.swiper--core-features', {
+const swiperCoreFeatures = new Swiper('.swiper--core-features', {
     direction: "horizontal",
     loop: true,
     slidesPerView: 1,
@@ -23,20 +13,44 @@ const swiper = new Swiper('.swiper--handpicked-rooms', {
  
     
 });
-   
+const swiperMenu = new Swiper('.swiper--menu', {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+
+  navigation: {
+    nextEl: menuNextButton,
+    prevEl: menuPrevButton, 
+  },
+
   
-  
- const swiperCoreFeaturesDark = new Swiper('.swiper__core-features--dark', {
-    direction: "horizontal",
-    loop: true,
-    slidesPerView: 1,
- 
-    pagination: {
-      el: '.swiper-pagination__core-features--dark',
-      clickable: true,
-    },
- 
-    
 });
+
+ 
+ const swiperMenuPhoto = new Swiper('.swiper--menu__photo', {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+
+  pagination: {
+    el: '.swiper-pagination--menu__photo',
+    clickable: true,
+  },
+
+  
+});
+
+ const swiperCoreFeaturesDark = new Swiper('.swiper__core-features--dark', {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+
+  pagination: {
+    el: '.swiper-pagination__core-features--dark',
+    clickable: true,
+  },
+
+  
+ });
 
    
