c878f5f721c863b39795bffa05624a6d4db38dc1
josekbm <jcblanco91@outlook.com>
2023-06-20 18:50:26 +0200

    Home finished

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,18 +1,6 @@
 const menuPrevButton = document.querySelector('.menu__button-prev');
 const menuNextButton = document.querySelector('.menu__button-next');
 
-const swiperCoreFeatures = new Swiper('.swiper--core-features', {
-    direction: "horizontal",
-    loop: true,
-    slidesPerView: 1,
- 
-    pagination: {
-      el: '.swiper-pagination--core-features',
-      clickable: true,
-    },
- 
-    
-});
 const swiperMenu = new Swiper('.swiper--menu', {
   direction: "horizontal",
   loop: true,
@@ -25,9 +13,8 @@ const swiperMenu = new Swiper('.swiper--menu', {
 
   
 });
-
  
- const swiperMenuPhoto = new Swiper('.swiper--menu__photo', {
+const swiperMenuPhoto = new Swiper('.swiper--menu__photo', {
   direction: "horizontal",
   loop: true,
   slidesPerView: 1,
@@ -39,18 +26,14 @@ const swiperMenu = new Swiper('.swiper--menu', {
 
   
 });
-
- const swiperCoreFeaturesDark = new Swiper('.swiper__core-features--dark', {
-  direction: "horizontal",
+const swiper = new Swiper('.swiper_facilities', {
+  // Optional parameters
+  direction: 'horizontal',
   loop: true,
-  slidesPerView: 1,
-
+  
+  // If we need pagination
   pagination: {
-    el: '.swiper-pagination__core-features--dark',
-    clickable: true,
+    el: '.swiper-pagination__facilities',
   },
 
-  
- });
-
-   
+});
